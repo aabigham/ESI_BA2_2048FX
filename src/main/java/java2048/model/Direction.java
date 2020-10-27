cfe2eb843d3dc251a8fc709d1b3b45da7a30a3c7
Amine-Ayoub Bigham <aminebigham@hotmail.com>
2020-10-27 16:41:59 +0100

    direction class

---
+++ src/main/java/java2048/model/Direction.java
@@ -0,0 +1,66 @@
+package java2048.model;
+
+/**
+ * Directions in which the board is going to slide the tiles to.
+ *
+ * @author Amine-Ayoub Bigham {@literal <g54985@etu.he2b.be>}
+ */
+public enum Direction {
+    NORTH(-1, 0),
+    SOUTH(1, 0),
+    EAST(0, 1),
+    WEST(0, -1);
+
+    private final int deltaRow;
+    private final int deltaCol;
+
+    /**
+     * Constructor of Direction.
+     *
+     * @param deltaRow the delta row.
+     * @param deltaCol the delta column.
+     */
+    private Direction(int deltaRow, int deltaCol) {
+        this.deltaRow = deltaRow;
+        this.deltaCol = deltaCol;
+    }
+
+    /**
+     * Function that returns the opposite direction of the current one.
+     *
+     * @return the opposite direction.
+     */
+    public Direction opposite() {
+        switch (this) {
+            case NORTH:
+                return SOUTH;
+            case SOUTH:
+                return NORTH;
+            case EAST:
+                return WEST;
+            case WEST:
+                return EAST;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    /**
+     * Get the value of deltaRow
+     *
+     * @return the value of deltaRow
+     */
+    public int getDeltaRow() {
+        return deltaRow;
+    }
+
+    /**
+     * Get the value of deltaCol
+     *
+     * @return the value of deltaCol
+     */
+    public int getDeltaCol() {
+        return deltaCol;
+    }
+
+}
