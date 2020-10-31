c4b771033c4c480f8e0bd94747963fc3969cd5e1
Amine-Ayoub Bigham <aminebigham@hotmail.com>
2020-10-31 11:51:04 +0100

    ask direction method

--- src/main/java/java2048/controller/Controller.java
+++ src/main/java/java2048/controller/Controller.java
@@ -5,6 +5,8 @@
  */
 package java2048.controller;
 
+import java.util.Scanner;
+import java2048.model.Direction;
 import java2048.model.Game;
 import java2048.view.InterfaceView;
 
@@ -31,5 +33,35 @@ public class Controller {
     public void startGame() {
         game.initGame();
         consoleView.displayBoard(game.getBoard());
+    }
+
+    /**
+     * Asks a direction.
+     *
+     * @return the direction.
+     */
+    Direction askDirection() {
+        Scanner scanner = new Scanner(System.in);
+        consoleView.displayMessage("Enter your direction : Z (UP) | Q (LEFT) | S (DOWN) | D (RIGHT)");
+        char c = scanner.nextLine().charAt(0);
+        Character.toLowerCase(c);
+
+        while (c != 'z' && c != 'q' && c != 's' && c != 'd') {
+            consoleView.displayMessage("Enter your direction : Z (UP) | Q (LEFT) | S (DOWN) | D (RIGHT)");
+            c = scanner.nextLine().charAt(0);
+        }
+
+        switch (c) {
+            case 'z':
+                return Direction.NORTH;
+            case 'q':
+                return Direction.WEST;
+            case 's':
+                return Direction.EAST;
+            case 'd':
+                return Direction.EAST;
+            default:
+                throw new AssertionError();
+        }
     }
 }
